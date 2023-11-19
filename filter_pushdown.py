fd846aca60e9c1de9836bac650f2383d54dccf15
Cijie Xia <cijie.xia@mail.utoronto.ca>
2023-11-19 16:58:19 -0500

    update

--- filter_pushdown.py
+++ filter_pushdown.py
@@ -7,44 +7,43 @@ ra = RelationalAlgebra(table_name)
 def filter_predicate(row):
     return int(row[ra._Schema().index("Founded")]) > 1990 
 
-# Unoptimized Query 
 
-# Project
-#   Filter 
-#     Sort
-#       Scan 
-start_time1 = time.time()
+def run_unoptimized_query():
+    # Unoptimized Query 
 
-ra.Project(
-        ra.Filter(
-            ra.Sort(
-                ra.Scan(), "Number of employees"
-            ), 
-            filter_predicate
+    # Project
+    #   Filter 
+    #     Sort
+    #       Scan 
+
+
+    ra.Project(
+            ra.Filter(
+                ra.Sort(
+                    ra.Scan(), "Number of employees"
+                ), 
+                filter_predicate
+            ),
+            ["Name"]
+    )
+
+def run_optimized_query():
+    # optimized Query 
+
+    # Project
+    #   Sort
+    #     Filter
+    #       Scan 
+
+
+
+    ra.Project(
+        ra.Sort(
+            ra.Filter(
+                ra.Scan(),
+                filter_predicate
+            ),
+            "Number of employees"
         ),
         ["Name"]
-)
-
-print("--- %s seconds ---" % (time.time() - start_time1))
-
-# optimized Query 
-
-# Project
-#   Sort
-#     Filter
-#       Scan 
-
-start_time2 = time.time()
-
-ra.Project(
-    ra.Sort(
-        ra.Filter(
-            ra.Scan(),
-            filter_predicate
-        ),
-        "Number of employees"
-    ),
-    ["Name"]
-)
-
-print("--- %s seconds ---" % (time.time() - start_time2))
+    )
